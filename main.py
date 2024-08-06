03ee840c1b8e98e131a1bcab89f07a0b23b2b913
Nimajavanbakht02 <nimabrah19@gmail.com>
2024-08-06 12:49:02 -0700

    creates pieces and locations

--- main.py
+++ main.py
@@ -5,13 +5,35 @@ pygame.init()
 #! game specs
 WIDTH = 1000
 HEIGHT = 900
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Two-Player Pygame Chess!')
 font = pygame.font.Font('freesansbold.ttf', 20)
 big_font = pygame.font.Font('freesansbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60
 
+#! game variables and images
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
 
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0),
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook',
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7),
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+
+captured_pieces_white = []
+captured_pieces_black = []
+
+#! 0 - whites turn, no selection: 1 - whites turn, piece selected:
+#! 2 - blacks turn,  no selection: 3 - blacks turn, piece selected
+turn_step = 0
+selection = 100
+valid_moves = []
+#! load in game piece images (queen, king, rook, bishop, knight, pawn) x2
 
 #! main game loop
 run = True
